0d2f42316bbdd9f2b133dec8869febfb7b384611
kantraman <93331484+kantraman@users.noreply.github.com>
2022-02-03 12:32:38 +0530

    Corrected error in crypto.js

--- src/helpers/crypto.js
+++ src/helpers/crypto.js
@@ -6,20 +6,20 @@ const algorithm = "aes-256-cbc";
 const initVector = crypto.randomBytes(16);
 // secret key generate 32 bytes of random data
 const Securitykey = crypto.randomBytes(32);
-// the cipher function
-const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+
 
 const encryptData = (message) => {
+    // the cipher function
+    const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
     let encryptedData = cipher.update(message, "utf-8", "hex");
     encryptedData += cipher.final("hex");
     console.log("Encrypted message: " + encryptedData);
     return encryptedData;
 }
 
-// the decipher function
-const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
-
 const decryptData = (encryptedData) => {
+    // the decipher function
+    const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
     let decryptedData = decipher.update(encryptedData, "hex", "utf-8");
     decryptedData += decipher.final("utf8");
     console.log("Decrypted message: " + decryptedData);
